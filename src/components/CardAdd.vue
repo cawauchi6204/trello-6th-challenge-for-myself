1a67053c8c1eaa23c490c71438db354ab4fa38b9
cawauchi6204 <cawauchi6204@gmail.com>
2020-05-09 09:59:47 +0900

    ファイル名を変更

---
+++ src/components/CardAdd.vue
@@ -0,0 +1,31 @@
+<template>
+  <form 
+    @submit.prevent="addCardToList" 
+    class="addcard"
+  >
+    <input 
+      type="text"
+      class="text-input"
+      placeholder="Add new card"
+      v-model="body"
+      >
+      <button 
+        type="submit"
+        class="add-button"
+        >Add</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      body:''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
